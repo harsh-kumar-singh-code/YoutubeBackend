e85a8659aa97b77cea22a6879eb940dfb36e35f9
Harsh kumar singh <harshstudyfunda@gmail.com>
2026-02-14 15:39:07 +0530

    little

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -49,13 +49,16 @@ const registerUser = asyncHandler(async (req,res,next)=> {
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
 
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
 
     let coverImageLocalPath;
 
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
-    console.log(req.files);
+
+    //console.log(req.files);
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar is required")
