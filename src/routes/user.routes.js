1ac34d34c49bd51fce5e87dd641200930364fb67
Harsh kumar singh <harshstudyfunda@gmail.com>
2026-02-15 16:34:13 +0530

    Add missing user profile management functionalities to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,7 @@
 import {Router } from "express"
-import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js"
+import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { verify } from "jsonwebtoken";
 
 
 const router = Router()
